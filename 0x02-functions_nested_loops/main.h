568f52877f2c321567428f1f8dc8c591a173ea20
WiltonMashele <Wiltonmashele@gmail.com>
2023-02-21 10:54:08 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,40 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* _putchar - writes the character c to stdout */
+int _putchar(char c);
+
+/* print_alphabet - prints the alphabet in lowercase */
+void print_alphabet(void);
+
+/* print_alphabet_x10 - prints the alphabet in lowercase 10 times */
+void print_alphabet_x10(void);
+
+/* _islower - checks for lowercase character */
+int _islower(int c);
+
+/* _isalpha - checks for alphabetic character */
+int _isalpha(int c);
+
+/* print_sign - prints the sign of a number */
+int print_sign(int n);
+
+/* _abs - computes the absolute value of an integer */
+int _abs(int);
+
+/* print_last_digit - prints the last digit of a number */
+int print_last_digit(int);
+
+/* jack_bauer - prints every minute of the day of Jack Bauer */
+void jack_bauer(void);
+
+/* times_table - prints the 9 times table, starting with 0 */
+void times_table(void);
+
+/* add - adds two integers */
+int add(int, int);
+
+/* print_to_98 - prints all natural numbers from n to 98 */
+void print_to_98(int n);
+
+#endif /* MAIN_H */
