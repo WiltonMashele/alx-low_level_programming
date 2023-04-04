17e76666c2852d16adf46bfb638b87b5e4bf9701
WiltonMashele <Wilton.mashele@gmail.com>
2023-04-04 12:28:03 -0700

    a function that prints all the elements of a listint_t list

---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,62 @@
+#ifndef STDLIB_H
+#define STDLIB_H
+#include <stdlib.h>
+#endif
+
+#ifndef _LISTS_H
+#define _LISTS_H
+
+/**
+*
+* struct list_s - Singly linked list
+* @str: Pointer to a string - (malloc'ed string)
+* @len: Length of the string
+* @next: Points to the next node
+* Description: This structure defines a node in a singly linked list.
+* It contains a pointer to a dynamically allocated string, the length of the string,
+* and a pointer to the next node in the list.
+*/
+typedef struct list_s
+{
+char *str;
+unsigned int len;
+struct list_s *next;
+} list_t;
+
+/* Function prototypes for singly linked list */
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
+
+/**
+*
+* struct listint_s - Singly linked list
+* @n: Integer data
+* @next: Points to the next node
+* Description: This structure defines a node in a singly linked list.
+* It contains an integer data and a pointer to the next node in the list.
+*/
+typedef struct listint_s
+{
+int n;
+struct listint_s *next;
+} listint_t;
+
+/* Function prototypes for singly linked list with integer data */
+size_t print_listint(const listint_t *h);
+size_t listint_len(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
+listint_t *add_nodeint_end(listint_t **head, const int n);
+void free_listint(listint_t *head);
+void free_listint2(listint_t **head);
+int pop_listint(listint_t **head);
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
+int sum_listint(listint_t *head);
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
+int delete_nodeint_at_index(listint_t **head, unsigned int index);
+listint_t *reverse_listint(listint_t **head);
+size_t print_listint_safe(const listint_t *head);
+
+#endif
