1d4425b11d172bb0ca529fb829ea0a6f773b1b4b
WiltonMashele <Wilton.mashele@gmail.com>
2023-04-04 12:57:22 -0700

    lists.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,34 +1,5 @@
-#ifndef STDLIB_H
-#define STDLIB_H
-#include <stdlib.h>
-#endif
-
 #ifndef _LISTS_H
 #define _LISTS_H
-
-/**
-*
-* struct list_s - Singly linked list
-* @str: Pointer to a string - (malloc'ed string)
-* @len: Length of the string
-* @next: Points to the next node
-* Description: This structure defines a node in a singly linked list.
-* It contains a pointer to a dynamically allocated string, the length of the string,
-* and a pointer to the next node in the list.
-*/
-typedef struct list_s
-{
-char *str;
-unsigned int len;
-struct list_s *next;
-} list_t;
-
-/* Function prototypes for singly linked list */
-size_t print_list(const list_t *h);
-size_t list_len(const list_t *h);
-list_t *add_node(list_t **head, const char *str);
-list_t *add_node_end(list_t **head, const char *str);
-void free_list(list_t *head);
 
 /**
 *
