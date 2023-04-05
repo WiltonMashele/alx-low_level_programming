1b4651ae5d2b397a19d3fb4106063429f2641916
WiltonMashele <Wilton.mashele@gmail.com>
2023-04-04 17:32:14 -0700

    a function that returns the nth node of a listint_t linked list.

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -5,7 +5,8 @@
  * @head: pointer to the head of the linked list
  * @index: index of the node to retrieve
  *
- * Return: pointer to the nth node of the linked list, or NULL if it doesn't exist
+ * Return: pointer to the nth node of the linked list,
+ * or NULL if it doesn't exist
  */
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
