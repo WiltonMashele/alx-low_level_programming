31089449f24491c6d4347ac6c05288451a0545ab
WiltonMashele <Wilton.mashele@gmail.com>
2023-04-11 14:54:42 -0700

    debugging

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -13,7 +13,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 
 {
 	unsigned long int xor_result = n ^ m;
-	unsigned int count;
+	unsigned int count = 0;
 
 	while (xor_result != 0)
 	{
