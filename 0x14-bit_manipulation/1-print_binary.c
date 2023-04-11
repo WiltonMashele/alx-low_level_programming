cee6b98f6c6bccae29403522799550578fbb1d56
WiltonMashele <Wilton.mashele@gmail.com>
2023-04-11 16:03:53 -0700

    debugging

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -3,27 +3,24 @@
 
 /**
  * print_binary - prints the binary representation of a number.
- * bit - to store value of a single bit
+ * num_bits - calculates the number of bits
  * @n : input number
- * Shift the mask to the right by 1 bit
+ * 
  */
+
+/*helper function to print a single character*/
+void _putchar(char c)
+{
+	putchar(c)
+}
 
 void print_binary(unsigned long int n)
 
 {
-	int size = sizeof(unsigned long int) * 8;
+	int num_bits = sizeof(unsigned long int) * 8;
 
-	for (int i = size - 1; i >= 0; i--)
+	for (int i = num_bits - 1; i >= 0; i--)
 	{
-	unsigned long bit = (n >> 1) & 1;
-
-	if (bit)
-	{
-	write(1, "1", 1);
-	}
-	else
-	{
-	write(1, "0", 1);
-	}
+		_putchar(((n >> i) & 1) + '0');
 	}
 }
