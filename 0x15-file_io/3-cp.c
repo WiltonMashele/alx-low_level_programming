db07c681e856dc7f3ef02e010b19884878ee0261
WiltonMashele <Wilton.mashele@gmail.com>
2023-04-18 04:11:21 -0700

    a program that copies the content of a file to another file.

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,3 +1,4 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
@@ -6,58 +7,58 @@
 #include <sys/types.h>
 
 /**
+ * error - print an error message and exit the program with status 1.
+ *
+ * @msg: the error message to print.
+ */
+
+void error(char *msg)
+{
+	dprintf(STDERR_FILENO, "%s\n", msg),
+		exit(1);
+}
+
+/**
  * main - a program that copies the content of a file to another file.
  *
-
+ * @argc: the number of command-line arguments.
+ * @argv: an array of command-line argument strings.
+ *
+ * Return: 0 on success, 1 on error.
+ */
 
 int main(int argc, char *argv[])
+
 {
-    int file_from;
-    int file_to;
-    int num1 = 1024;
-    int num2 = 0;
-    char buf[1024];
+	int file_from, file_to, num1, num2;
+	char buf[1024];
 
-    if (argc != 3) {
-        dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-        exit(97);
-    }
+	if (argc != 3)
+	error("Usage: cp file_from file_to");
 
-    file_from = open(argv[1], O_RDONLY);
-    if (file_from == -1) {
-        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-        exit(98);
-    }
+	file_from = open(argv[1], O_RDONLY);
 
-    file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
-    if (file_to == -1) {
-        dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-        close(file_from);
-        exit(99);
-    }
+	if (file_from == -1)
+	error("Error: Can't read from file");
 
-    while (num1 == 1024) {
-        num1 = read(file_from, buf, 1024);
-        if (num1 == -1) {
-            dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-            exit(98);
-        }
-        num2 = write(file_to, buf, num1);
-        if (num2 < num1) {
-            dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-            exit(99);
-        }
-    }
+	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR |
+			S_IRGRP | S_IWGRP | S_IROTH);
 
-    if (close(file_from) == -1) {
-        dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
-        exit(100);
-    }
+	if (file_to == -1)
+	error("Error: Can't write to file");
 
-    if (close(file_to) == -1) {
-        dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
-        exit(100);
-    }
+	while ((num1 = read(file_from, buf, 1024)) > 0)
+	{
+	num2 = write(file_to, buf, num1);
+	if (num2 != num1)
+	error("Error: Can't write to file");
+	}
 
-    return 0;
+	if (close(file_from) == -1)
+	error("Error: Can't close file_from");
+
+	if (close(file_to) == -1)
+	error("Error: Can't close file_to");
+
+	return (0);
 }
