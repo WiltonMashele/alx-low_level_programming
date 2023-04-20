a260e051a45ce5c923b5796d7e88b24c8a96220e
WiltonMashele <Wilton.mashele@gmail.com>
2023-04-20 05:59:32 -0700

    debugging

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,37 +1,19 @@
 #include "main.h"
 #include <stdio.h>
 
- /**
-  * _putchar - helps function to print a single character.
-  * @c: character to be printed.
-  * Return: On success, the character printed as an unsigned char
-  * On error, -1 is returned, and errno is set appropriately.
-  */
-
-int _putchar(char c)
-{
-	putchar(c);
-}
-
 /**
  * print_binary - prints the binary representation of a number.
- * num_bits - the number of bits in unsigned long int
- * @ >> i: shift right and AND with 1 to get the value bit
  * and prints as '0' or '1'.
  * @n: unsigned long int number to be printed in binary.
  * This function takes an unsigned long int number and prints.
  */
 
 void print_binary(unsigned long int n)
+
 {
-	int num_bits;
-	int i;
-
-	num_bits = sizeof(unsigned long int) * 8;
-
-	/*start from the most significant bit and print each bit*/
-	for (i = num_bits - 1; i >= 0; i--)
+	if (n > 1)
 	{
-		_putchar(((n >> i) & 1) + '0');
+	print_binary(n >> 1);
 	}
+	 _putchar((n & 1) + '0');
 }
