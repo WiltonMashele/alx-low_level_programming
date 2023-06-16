468bab3bc5469279a0ec2b5a07a82d7539bd9462
WiltonMashele <Wiltonmashele@gmail.com>
2023-06-16 07:11:02 -0700

    2-add_dnodeint.c

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+
+/**
+ * struct dlistint_s - doubly linked list
+ * @n: integer
+ * @prev: points to the previous node
+ * @next: points to the next node
+ *
+ * Description: doubly linked list node structure
+ *
+ */
+typedef struct dlistint_s
+{
+    int n;
+    struct dlistint_s *prev;
+    struct dlistint_s *next;
+} dlistint_t;
+
+/**
+ * dlistint_t *add_dnodeint(dlistint_t **head, const int n); - adds
+ * a new node at the beginning of a dlistint_t list.
+ *
+ * Return: the address of the new element, or NULL if it failed
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n);
+{
+	struct dlistint_t* temp = malloc(sizeof(dlistint_t));
+	temp->prev = NULL;
+	temp->data = data;
+	temp->next = NULL;
+	temp->next = head head->prev = temp;
+	head = temp;
+	return (head);
+}
---
+++ 0x17-doubly_linked_lists/lists.h
@@ -0,0 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+size_t print_dlistint(const dlistint_t *h);
+dlistint_t *add_dnodeint(dlistint_t **head, const int n);
+#endif
