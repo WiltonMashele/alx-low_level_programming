97ef529e789eefafc25572cb0440f7af3b8b6733
WiltonMashele <Wiltonmashele@gmail.com>
2023-07-09 02:47:15 -0700

    fixing 2-key_index.c

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -5,8 +5,8 @@
  * 
  * Returns the index of a key in the hash table.
  *
- * @key:  The key to retrieve the index for.
- * @size: The size of the hash table.
+ * key:  The key to retrieve the index for.
+ * size: The size of the hash table.
  *
  * Returns: The index of the key in the hash table.
  */
@@ -14,8 +14,7 @@
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	unsigned long int hash = hash_djb2(key);
-
 	unsigned long int index = hash % size;
 
-	return (index);
+	return(index);
 }
