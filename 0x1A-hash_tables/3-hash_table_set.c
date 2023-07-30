6dfa4e4c8cecdf8d9a7cd1948f72d8e27b7a5e35
WiltonMashele <Wiltonmashele@gmail.com>
2023-07-30 13:06:41 -0700

    fixing

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -15,23 +15,25 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new_node;
 	char *value_copy;
 	unsigned long int index, collision_index;
+
 	index = key_index((const unsigned char *)key, ht->size);
 
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
-		return(0);
+		return (0);
 
 	value_copy = strdup(value);
 
 	if (value_copy == NULL)
-		return(0);
+		return (0);
 
-	for (collision_index = index; ht->array[collision_index] != NULL; collision_index++)
+	for (collision_index = index;
+			ht->array[collision_index] != NULL; collision_index++)
 	{
 		if (strcmp(ht->array[collision_index]->key, key) == 0)
 		{
 			free(ht->array[collision_index]->value);
 			ht->array[collision_index]->value = value_copy;
-			return(1);
+			return (1);
 		}
 	}
 
@@ -39,17 +41,17 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (new_node == NULL)
 	{
 		free(value_copy);
-		return(0);
+		return (0);
 	}
 	new_node->key = strdup(key);
 	if (new_node->key == NULL)
 	{
 		free(new_node);
-		return(0);
+		return (0);
 	}
 	new_node->value = value_copy;
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
 
-	return(1);
+	return (1);
 }
