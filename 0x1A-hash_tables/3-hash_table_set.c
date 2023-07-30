92daacec1a3fa45b909ff9791a347b257f37ab5c
WiltonMashele <Wiltonmashele@gmail.com>
2023-07-30 12:56:22 -0700

    fixing

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -15,16 +15,15 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new_node;
 	char *value_copy;
 	unsigned long int index, collision_index;
+	index = key_index((const unsigned char *)key, ht->size);
 
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return(0);
 
-	char *value_copy = strdup(value);
+	value_copy = strdup(value);
 
 	if (value_copy == NULL)
 		return(0);
-
-	index = key_index((const unsigned char *)key, ht->size);
 
 	for (collision_index = index; ht->array[collision_index] != NULL; collision_index++)
 	{
@@ -36,7 +35,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		}
 	}
 
-	hash_node_t *new_node = malloc(sizeof(hash_node_t));
+	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 	{
 		free(value_copy);
