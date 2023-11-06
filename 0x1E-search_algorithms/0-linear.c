5bf96ad561d00d5f5660be66cd0fc8aea61a8c55
WiltonMashele <Wiltonmashele@gmail.com>
2023-11-06 10:16:45 -0800

    fixing

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,15 +1,15 @@
 #include "search_algos.h"
+
 /**
- * linear_skip - Searches for a value in a sorted singly linked list of integers
- *               using the linear skip algorithm.
- * @list: A pointer to the head of the linked list to search.
+ * linear_search - Searches for a value in an array using the linear search algorithm.
+ * @array: Pointer to the array to search.
+ * @size: Number of elements in the array.
  * @value: The value to search for.
  *
- * Return: If the value is not found or the head of the list is NULL, return NULL.
- *         Otherwise, return a pointer to the first node where the value is found.
+ * Return: If the value is found, return the index where it's located. If not found,
+ * return -1.
  *
- * Description: This function prints the value every time it is compared in the list
- *              and uses the square root of the list size as the jump step.
+ * Description: This function iterates through the array, printing each compared value.
  */
 int linear_search(int *array, size_t size, int value)
 {
